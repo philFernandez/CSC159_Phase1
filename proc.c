35129c019ddfa25a66862bb6cab3a58b72fdf8ca
philFernandez <norcal618@gmail.com>
2020-02-01 01:03:33 -0800

    did some proc

--- proc.c
+++ proc.c
@@ -2,39 +2,48 @@
 // all user processes are coded here
 // processes do not R/W kernel data or call kernel code, only via syscalls
 
+#include "kernel.h" // was using <kernel.h>
 #include <spede.h>
-#include <kernel.h>
+#include <stdio.h>
+#include <string.h>
 
-/*prototype any subroutines of Clock() here*/
+void numberToString(int, char *);
+int strLen(char *);
 
-
-void Clock() {
-   int i;
-   char str[] = "    ";  // str for 4-digits, plus NUL
-   unsigned short *p;
-   while(1) {
-
-   }
+void Clock()
+{
+    int i;
+    char str[] = "    "; // str for 4-digits, plus NUL
+    unsigned short *p;
+    while (1)
+    {
+    }
 }
 
+/*program a void - return function Clock that takes no argument*/
+/*{*/
 
-program a void-return function Clock that takes no argument {
+/*an infinite loop*/
+/*: if sys_tick is now at a new second convert the current second count*/
+/*number to str set p to VIDEO_START(1st row, 1st column)*/
+/*advance p by 75 columns(to the right, on the same row)*/
 
-   an infinite loop:
-      if sys_tick is now at a new second
-         convert the current second count number to str
-         set p to VIDEO_START (1st row, 1st column)
-         advance p by 75 columns (to the right, on the same row)
+/*loop thru each char in str*/
+/*: *p = the character + VIDEO_MASK*/
+/*}*/
 
-         loop thru each char in str:
-            *p = the character + VIDEO_MASK
+/*
+ * Takes number and reference to str. str is populated with number
+ */
+void numberToString(int number, char *str)
+{
+    sprintf(str, "%d", number);
 }
 
-Program a void-return function:
-given a number and an address of a string, it converts the number
-to the string.
-
-Program an integer-return function:
-given a string, the function counts up the length of the string
-and returns it.
-
+/*
+ * Return str length
+ */
+int strLen(char *str)
+{
+    return strlen(str);
+}
